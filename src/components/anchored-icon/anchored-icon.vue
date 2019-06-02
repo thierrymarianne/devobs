027f4a70a0962181876f1c73f629115a52648319
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2019-06-02 12:54:54 +0200

    Introduced anchored icon component

---
+++ src/components/anchored-icon/anchored-icon.vue
@@ -0,0 +1,58 @@
+<template>
+  <span :class="classes.wrapperClasses">
+    <a v-if="url" :href="url" :class="classes.anchorClasses" target="_blank">
+      <font-awesome-icon :icon="icons" :class="classes.iconClasses" />
+    </a>
+    <font-awesome-icon v-else :icon="icons" :class="classes.iconClasses" />
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'anchored-icon',
+  props: {
+    wrapperClasses: {
+      type: Object,
+      default: () => ({})
+    },
+    url: {
+      type: String,
+      default: ''
+    },
+    iconClasses: {
+      type: Object,
+      default: () => ({})
+    },
+    icons: {
+      type: Array,
+      required: true
+    },
+    anchorClasses: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+      classes: {
+        wrapperClasses: {
+          'anchored-icon': true,
+          ...this.wrapperClasses
+        },
+        iconClasses: {
+          'anchored-icon__icon': true,
+          ...this.iconClasses
+        },
+        anchorClasses: {
+          'anchored-icon__anchor': true,
+          ...this.anchorClasses
+        }
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import './anchored-icon.scss';
+</style>
