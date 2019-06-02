42f8bd2a16b70a9c21489ff32467333bb1da3d4a
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2019-06-02 17:30:02 +0200

    Warn about invalid session

--- src/modules/errors.js
+++ src/modules/errors.js
@@ -1,5 +1,14 @@
 class NoRemainingStatusAfterApplyingFilter extends Error {}
+class InvalidSession extends Error {
+  constructor(
+    message = `Have your mapped the "getIdToken" authentication getter
+    from the component sending an HTTP request?`
+  ) {
+    super(message);
+  }
+}
 
 export default {
+  InvalidSession,
   NoRemainingStatusAfterApplyingFilter
 };
