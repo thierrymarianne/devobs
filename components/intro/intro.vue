996518e38850ef7e4c8a4c45bd00af5e4c614b9b
Thierry Marianne <thierry@marianne.io>
2021-01-06 23:17:36 +0100

    Render affiliated websites selector

--- components/intro/intro.vue
+++ components/intro/intro.vue
@@ -12,6 +12,79 @@
           />
         </a>
       </div>
+      <div
+        v-if="affiliatedWebsites.length > 0"
+        class="intro__description-container"
+      >
+        <form
+          v-if="showLightSelector"
+          class="intro__affiliated-websites"
+          method="get"
+          ref="websiteSelector"
+          :action="selectedWebsiteLink"
+        >
+          <label
+            for="affiliated-websites"
+            class="intro__affiliated-websites-label"
+          >
+              <span class="intro__affiliated-websites-label-prefix"><!--
+                -->Browse other snapshots
+              </span>
+            <select
+              id="affiliated-websites"
+              v-model="selectedWebsite"
+              v-on:change="changeWebsite(selectedWebsite)"
+              class="intro__affiliated-websites-selector"
+            >
+              <option
+                class="intro__affiliated-websites-option"
+                v-for="(website, index) in affiliatedWebsites"
+                :key="index"
+                :value="website.hostname"
+                v-text="`https://${website.hostname}`"
+              >
+              </option>
+            </select>
+          </label>
+        </form>
+        <form
+          class="intro__affiliated-websites"
+          v-else
+        >
+          <fieldset class="intro__affiliated-websites-fieldset">
+            <legend>Daily Snapshots</legend>
+            <label
+              class="intro__affiliated-websites-label"
+            >
+              <span
+                class="intro__affiliated-websites-label-prefix">
+                You will find other daily snapshots at
+              </span>
+              <select
+                v-model="selectedWebsite"
+                class="intro__affiliated-websites-selector"
+              >
+                <option
+                  v-for="(website, index) in affiliatedWebsites"
+                  :key="index"
+                  :value="website.hostname"
+                  v-text="`https://${website.hostname}`"
+                >
+                </option>
+              </select>
+            </label>
+            <a
+              v-if="selectedWebsiteLink"
+              :class="affiliatedWebsitesLinkClasses"
+              v-text="selectedWebsiteLinkLabel"
+              :href="selectedWebsiteLink"
+            />
+            <span
+              class="intro__visiting"
+              v-else v-text="selectedWebsiteLinkLabel" />
+          </fieldset>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -21,9 +94,41 @@ import logo from "~/assets/logo.png";
 
 export default {
   name: "intro",
+  props: {
+    affiliatedWebsites: {
+      type: Array,
+      default: []
+    },
+    showLightSelector: {
+      type: Boolean,
+      default: true
+    }
+  },
+  computed: {
+    affiliatedWebsitesLinkClasses() {
+      return {
+        'intro__external-link': true,
+        'intro__external-link--visible': this.selectedWebsite,
+      };
+    },
+    selectedWebsiteLink() {
+      return this.selectedWebsite ? `https://${this.selectedWebsite}` : null;
+    },
+    selectedWebsiteLinkLabel() {
+      return this.selectedWebsite
+        ? `Go to ${this.selectedWebsiteLink}`
+        : `You are currently visiting https://${window.location.hostname}`;
+    }
+  },
+  methods: {
+    changeWebsite(website) {
+      window.location = `https://${website}`;
+    }
+  },
   data() {
     return {
       currentRoute: this.$router.currentRoute.path,
+      selectedWebsite: null,
       logo
     }
   }
